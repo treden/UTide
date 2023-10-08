0f4140b42af89f780a690e67f501eb029317cb7f
Yann-Treden Tranchant <yann-treden.tranchant@csiro.au>
2023-10-08 17:43:51 +1100

    Adaptation of solver to the 2D case : first step

--- utide/_solve.py
+++ utide/_solve.py
@@ -118,7 +118,7 @@ def validate_order_constit(arg, have_snr):
     )
 
 
-def solve(t, u, v=None, lat=None, **opts):
+def solve(t, u, v=None, lat=None, solve = '1D', **opts):
     """
     Calculate amplitude, phase, confidence intervals of tidal constituents.
 
@@ -218,7 +218,11 @@ def solve(t, u, v=None, lat=None, **opts):
 
     compat_opts = _process_opts(opts, v is not None)
 
-    coef = _solv1(t, u, v, lat, **compat_opts)
+    # TODO: Proper transparent management of the 1D and 2D case with appropriates input's tests 
+    if solve == '1D':
+        coef = _solv1(t, u, v, lat, **compat_opts)
+    elif solve == '2D':
+        coef = _solv2(t, u, v, lat, **compat_opts)
 
     return coef
 
@@ -229,8 +233,8 @@ def _solv1(tin, uin, vin, lat, **opts):
     packed = _slvinit(tin, uin, vin, lat, **opts)
     tin, t, u, v, tref, lor, elor, opt = packed
     nt = len(t)
-    if opt["RunTimeDisp"]:
-        print("solve: ", end="")
+    # if opt["RunTimeDisp"]:
+    #     print("solve: ", end="")
 
     # opt['cnstit'] = cnstit
     cnstit, coef = ut_cnstitsel(
@@ -246,8 +250,8 @@ def _solv1(tin, uin, vin, lat, **opts):
     coef.aux.opt = opt
     coef.aux.lat = lat
 
-    if opt["RunTimeDisp"]:
-        print("matrix prep ... ", end="")
+    # if opt["RunTimeDisp"]:
+    #     print("matrix prep ... ", end="")
 
     ngflgs = [opt["nodsatlint"], opt["nodsatnone"], opt["gwchlint"], opt["gwchnone"]]
 
@@ -299,8 +303,8 @@ def _solv1(tin, uin, vin, lat, **opts):
 
     # nm = B.shape[1]  # 2*(nNR + nR) + 1, plus 1 if trend is included.
 
-    if opt["RunTimeDisp"]:
-        print("solution ... ", end="")
+    # if opt["RunTimeDisp"]:
+    #     print("solution ... ", end="")
 
     if opt["twodim"]:
         xraw = u + 1j * v
@@ -426,8 +430,224 @@ def _solv1(tin, uin, vin, lat, **opts):
     coef = _reorder(coef, opt)
     # This might have added PE if it was not already present.
 
-    if opt["RunTimeDisp"]:
-        print("done.")
+    # if opt["RunTimeDisp"]:
+    #     print("done.")
+
+    return coef
+
+def _solv2(tin, uin, vin, lat, **opts):
+    # The following returns a possibly modified copy of tin (ndarray).
+    # t, u, v are fully edited ndarrays (unless v is None).
+    packed = _slvinit(tin, uin, vin, lat, **opts)
+    tin, t, u, v, tref, lor, elor, opt = packed
+    nt = len(t)
+
+    # TODO: Put back the display option 
+    # if opt["RunTimeDisp"]:
+    #     print("solve: ", end="")
+
+    # opt['cnstit'] = cnstit
+    cnstit, coef = ut_cnstitsel(
+        tref,
+        opt["rmin"] / (24 * lor),
+        opt["cnstit"],
+        opt["infer"],
+    )
+
+    # a function we don't need
+    # coef.aux.rundescr = ut_rundescr(opt,nNR,nR,nI,t,tgd,uvgd,lat)
+
+    coef.aux.opt = opt
+    coef.aux.lat = lat
+
+    # if opt["RunTimeDisp"]:
+    #     print("matrix prep ... ", end="")
+
+    ngflgs = [opt["nodsatlint"], opt["nodsatnone"], opt["gwchlint"], opt["gwchnone"]]
+
+    E_args = (lat, ngflgs, opt.prefilt)
+
+    # Make the model array, starting with the harmonics.
+    E = ut_E(t, tref, cnstit.NR.frq, cnstit.NR.lind, *E_args)
+
+    # Positive and negative frequencies
+    B = np.hstack((E, E.conj()))
+
+    # TODO: Test/fix the 2D case with inference
+    if opt.infer is not None:
+        Etilp = np.empty((nt, coef.nR), dtype=complex)
+        Etilm = np.empty((nt, coef.nR), dtype=complex)
+
+        if not opt.infer.approximate:
+            for k, ref in enumerate(cnstit.R):
+                E = ut_E(t, tref, ref.frq, ref.lind, *E_args)
+                # (nt,1)
+                Q = ut_E(t, tref, ref.I.frq, ref.I.lind, *E_args) / E
+                # (nt,ni)
+                Qsum_p = (Q * ref.I.Rp).sum(axis=1)
+                Etilp[:, k] = E[:, 0] * (1 + Qsum_p)
+                Qsum_m = (Q * np.conj(ref.I.Rm)).sum(axis=1)
+                Etilm[:, k] = E[:, 0] * (1 + Qsum_m)
+
+        else:
+            # Approximate inference.
+            Q = np.empty((coef.nR,), dtype=float)
+            beta = np.empty((coef.nR,), dtype=float)
+
+            for k, ref in enumerate(cnstit.R):
+                E = ut_E(t, tref, ref.frq, ref.lind, *E_args)[:, 0]
+                Etilp[:, k] = E
+                Etilm[:, k] = E
+                num = ut_E(tref, tref, ref.I.frq, ref.I.lind, *E_args).real
+                den = ut_E(tref, tref, ref.frq, ref.lind, *E_args).real
+                Q[k] = (num / den)[0, 0]
+                arg = np.pi * lor * 24 * (ref.I.frq - ref.frq) * (nt + 1) / nt
+                beta[k] = np.sin(arg) / arg
+
+        B = np.hstack((B, Etilp, np.conj(Etilm)))
+
+    # add the mean
+    B = np.hstack((B, np.ones((nt, 1))))
+
+    if not opt["notrend"]:
+        B = np.hstack((B, ((t - tref) / lor)[:, np.newaxis]))
+
+    # nm = B.shape[1]  # 2*(nNR + nR) + 1, plus 1 if trend is included.
+
+    # if opt["RunTimeDisp"]:
+    #     print("solution ... ", end="")
+
+    if opt["twodim"]:
+        xraw = u + 1j * v
+    else:
+        xraw = u
+
+    if opt.newopts.method == "ols":
+        # Model coefficients.
+        try:
+            m = np.linalg.lstsq(B, xraw.T, rcond=None)[0]
+        except TypeError:
+            m = np.linalg.lstsq(B, xraw.T)[0]
+        W = np.ones(nt)  # Uniform weighting; we could use a scalar 1, or None.
+
+    # Utide2D : The robust fit is not handled (for now?)
+    # else:
+    #     rf = robustfit(B, xraw, **opt.newopts.robust_kw)
+    #     m = rf.b
+    #     W = rf.w
+    #     coef.rf = rf
+    coef.weights = W
+
+    xmod = np.dot(B, m).T  # Model fit.
+
+    if not opt["twodim"]:
+        xmod = np.real(xmod)
+
+    e = W * (xraw - xmod)  # Weighted residuals.
+
+    nI, nR, nNR = coef.nI, coef.nR, coef.nNR
+
+    ap = np.hstack((m[:nNR].T, m[2 * nNR : 2 * nNR + nR].T)).T
+    i0 = 2 * nNR + nR
+    am = np.hstack((m[nNR : 2 * nNR].T, m[i0 : i0 + nR].T)).T
+
+    Xu = np.real(ap + am)
+    Yu = -np.imag(ap - am)
+
+    if not opt["twodim"]:
+        coef["A"], _, _, coef["g"] = ut_cs2cep(Xu, Yu)
+        Xv = []
+        Yv = []
+
+    else:
+        Xv = np.imag(ap + am)
+        Yv = np.real(ap - am)
+        packed = ut_cs2cep(Xu, Yu, Xv, Yv)
+        coef["Lsmaj"], coef["Lsmin"], coef["theta"], coef["g"] = packed
+
+    # Mean and trend.
+    if opt["twodim"]:
+        if opt["notrend"]:
+            coef["umean"] = np.real(m[-1])
+            coef["vmean"] = np.imag(m[-1])
+        else:
+            coef["umean"] = np.real(m[-2])
+            coef["vmean"] = np.imag(m[-2])
+            coef["uslope"] = np.real(m[-1]) / lor
+            coef["vslope"] = np.imag(m[-1]) / lor
+    else:
+        if opt["notrend"]:
+            coef["mean"] = np.real(m[-1])
+        else:
+            coef["mean"] = np.real(m[-2])
+            coef["slope"] = np.real(m[-1]) / lor
+
+    if opt.infer:
+        # complex coefficients
+        apI = np.empty((nI,), dtype=complex)
+        amI = np.empty((nI,), dtype=complex)
+        ind = 0
+
+        for k, ref in enumerate(cnstit.R):
+            apI[ind : ind + ref.nI] = ref.I.Rp * ap[nNR + k]
+            amI[ind : ind + ref.nI] = ref.I.Rm * am[nNR + k]
+            ind += ref.nI
+
+        XuI = (apI + amI).real
+        YuI = -(apI - amI).imag
+
+        if not opt.twodim:
+            A, _, _, g = ut_cs2cep(XuI, YuI)
+            coef.A = np.hstack((coef.A, A))
+            coef.g = np.hstack((coef.g, g))
+        else:
+            XvI = (apI + amI).imag
+            YvI = (apI - amI).real
+            Lsmaj, Lsmin, theta, g = ut_cs2cep(XuI, YuI, XvI, YvI)
+            coef.Lsmaj = np.hstack((coef.Lsmaj, Lsmaj))
+            coef.Lsmin = np.hstack((coef.Lsmin, Lsmin))
+            coef.theta = np.hstack((coef.theta, theta))
+            coef.g = np.hstack((coef.g, g))
+
+    # TODO: 2D OLS Confidence intervals
+    # if opt["conf_int"]:
+    #     coef = _confidence(
+    #         coef,
+    #         cnstit,
+    #         opt,
+    #         t,
+    #         e,
+    #         tin,
+    #         elor,
+    #         xraw,
+    #         xmod,
+    #         W,
+    #         m,
+    #         B,
+    #         Xu,
+    #         Yu,
+    #         Xv,
+    #         Yv,
+    #     )
+
+    # Diagnostics.
+    # TODO: 2D Diagnostics 
+    # if not opt["nodiagn"]:
+    #     coef = ut_diagn(coef)
+    #     # Adds a diagn dictionary, always sorted by energy.
+    #     # This doesn't seem very useful.  Let's directly add the variables
+    #     # to the base coef structure.  Then they can be sorted with everything
+    #     # else.
+    #     coef["PE"] = _PE(coef)
+    #     coef["SNR"] = _SNR(coef)
+
+    # Re-order constituents.
+    # TODO: 2D Reordering 
+    # coef = _reorder(coef, opt)
+    # This might have added PE if it was not already present.
+
+    # if opt["RunTimeDisp"]:
+    #     print("done.")
 
     return coef
 
@@ -465,10 +685,16 @@ def _slvinit(tin, uin, vin, lat, **opts):
     if lat is None:
         raise ValueError("Latitude must be supplied")
 
-    # Supporting only 1-D arrays for now; we can add "group"
-    # support later.
-    if tin.shape != uin.shape or tin.ndim != 1 or uin.ndim != 1:
-        raise ValueError("t and u must be 1-D arrays")
+    # UTide2D : Remove the check for 1D u values (t must be still 1D)
+    # TODO: Check the shape of u (2D cpmsistent with the len of t vector)
+    # TODO: manage the v value for 2D currents
+    # TODO: Verify that the t vector is the same len of first u dim
+    
+    # if tin.shape != uin.shape or tin.ndim != 1 or uin.ndim != 1:
+    #     raise ValueError("t and u must be 1-D arrays")
+
+    if tin.ndim != 1:
+        raise ValueError("t must be 1-D array")
 
     if vin is not None and vin.shape != uin.shape:
         raise ValueError("v must have the same shape as u")
